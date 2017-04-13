a1fc96aa997fa8983947686d5bc7f7ead2923772
tuan anh hoang <tuan anh hoang>
2017-04-13 18:56:03 +0700

    Initial commit

---
+++ app/src/main/java/deptrailachuadu/com/bai3tahoccode/MainActivity.java
@@ -0,0 +1,13 @@
+package deptrailachuadu.com.bai3tahoccode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
